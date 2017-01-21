a29172c206c2a95a63a7ee4d15757e23e5158068
Matthew Fernandez <mlf22198910@gmail.com>
2017-01-21 18:08:36 -0500

    Virtual Pet Project

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtPet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Barry the Blowfish, please press yes to continue!");
+            string barry = Console.ReadLine();
+            if (barry.ToLower() == "yes")
+            {
+                Console.WriteLine("Barry the Blowfish is an Interactive Pet, you will feed him, give him water, and play with him");
+                Console.WriteLine("Barry has needs that you will assist him with, please press yes if you are up to the challenge");
+                string Barry = Console.ReadLine();
+                if (barry.ToLower() == "yes")
+                {
+                    BarrytheBlowfish();
+                }
+            }
+
+        }
+
+        static void BarrytheBlowfish()
+        {
+            while (true)
+            {
+                Console.WriteLine("1. Barry needs food, press 1 to feed him");
+                Console.WriteLine("2. Barry needs water, press 2 to give him something to drink");
+                Console.WriteLine("3. Barry is bored and wants to play. Press 3 to play with him");
+                Console.WriteLine("4. Barry is satisfied and needs no care!");
+                int answer = int.Parse(Console.ReadLine());
+                if (answer == 1)
+                {
+                    Console.WriteLine("Barry is full.He has full hunger of 10");
+                }
+                else if (answer == 2)
+                {
+                    Console.WriteLine("Barry is fully hydrated.He has full thirst of 10");
+                }
+                else if (answer == 3)
+                {
+                    Console.WriteLine("Barry is happy and energized. His boredom is full at 10");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Barry has been well taken care of. Thank you for caring for him and go back to your day");
+                }
+            }
+        }
+    }
+}
---
+++ virtualpet.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtPet
+{
+    class virtualpet
+    {
+        //fields
+        private string name = "Barry the Blowfish";
+        private int energy = 10;
+        private int hunger = 10;
+        private string color;
+        private int thirst = 10;
+        private int bored = 10;
+
+        //properties
+        public string Name
+        {
+            get { return this.name; }
+            set { this.color = value; }
+        }
+
+        public string Color
+        {
+            get { return this.color; }
+            set { this.color = value; }
+        }
+
+        public int Energy
+        {
+            get { return this.energy; }
+            set { this.energy = value; }
+        }
+
+        public int Hunger
+        {
+            get { return this.hunger; }
+            set { this.hunger = value; }
+        }
+
+        public int Thirst
+        {
+            get { return this.thirst; }
+            set { this.thirst = value; }
+        }
+
+        public int Bored
+        {
+            get { return this.bored; }
+            set { this.bored = value; }
+        }
+
+        //constructors
+
+        public void InteractivePet(int hunger, int energy, string color, int bored, int thirst)
+        {
+            this.hunger = hunger;
+            this.energy = energy;
+            this.color = color;
+            this.bored = bored;
+            this.thirst = thirst;
+        }
+
+        public void InteractivePet(int hunger, int energy, int bored, int thirst)
+        {
+            this.hunger = 10;
+            this.energy = 10;
+            this.bored = 10;
+            this.thirst = 10;
+        }
+
+        //method
+
+        public void ShowBarrytheBlowfish()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Color: " + color);
+            Console.WriteLine("Barry the Blowfish hunger level is: " + hunger);
+            Console.WriteLine("Barry the Blowfish energy level is: " + energy);
+            Console.WriteLine("Barry the Blowfish thirst level is: " + thirst);
+            Console.WriteLine("Barry the Blowfish bored level is: " + bored);
+        }
+    }
+}
+  
